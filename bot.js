8189e176d95864f2baa716a283cb0cc9f2e99a13
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-12-04 20:39:30 +0100

    buttons! and endededededed

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var token = '502137753:AAHDcKtIgwSFJQTmMiqnf2846avclZIqSA8'
+var token = '502137753:AAHDcKtIgwSFJQTmMiqnf2846avclZIqSA8';
 
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true } );
@@ -22,3 +22,54 @@ bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
     // reply sent!
   });
 });
+
+var option = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Some button text 1', callback_data: '1' }],
+      [{ text: 'Some button text 2', callback_data: '2' }],
+      [{ text: 'Some button text 3', callback_data: '3' }],
+      [{ text: 'END', callback_data: 'end' }]
+    ]
+  })
+};
+
+bot.onText(/^\/start$/, function (msg, match){
+  bot.sendMessage(msg.chat.id, 'START!').then(function () {
+    console.log('start');
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  });
+});
+
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+  };
+  var text;
+  if (action === 'end') {
+    text = 'Ended';
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "ENDEDEDEDEDEDEDEDEDED"); 
+  }
+  if (action === '1') {
+    text = 'You hit button 1';
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  }
+  if (action === '2') {
+    text = 'You hit button 2';
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  }
+  if (action === '3') {
+    text = 'You hit button 3';
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  }
+});
+
+
+
