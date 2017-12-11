db8920e3cfcc531782761b7cda2f4de0882f4201
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-12-11 19:20:39 +0100

    first try db

--- bot.js
+++ bot.js
@@ -29,6 +29,8 @@ var option = {
       [{ text: 'Some button text 1', callback_data: '1' }],
       [{ text: 'Some button text 2', callback_data: '2' }],
       [{ text: 'Some button text 3', callback_data: '3' }],
+      [{ text: 'Register user', callback_data: 'register_user' }],
+      [{ text: 'Write ids', callback_data: 'write_ids' }],
       [{ text: 'END', callback_data: 'end' }]
     ]
   })
@@ -52,7 +54,7 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
   if (action === 'end') {
     text = 'Ended';
     bot.editMessageText(text, opts);
-    bot.sendMessage(msg.chat.id, "ENDEDEDEDEDEDEDEDEDED"); 
+    bot.sendMessage(msg.chat.id, "END"); 
   }
   if (action === '1') {
     text = 'You hit button 1';
@@ -69,7 +71,30 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
     bot.editMessageText(text, opts);
     bot.sendMessage(msg.chat.id, "Select option", option); 
   }
+  if (action === 'register_user') {
+    register_user();
+    text = 'Now registering your name :)';
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  }
+  if (action === 'write_ids') {
+    text = 'chat_id' + chat_id + "\nmessage_id" + message_id;
+    bot.editMessageText(text, opts);
+    bot.sendMessage(msg.chat.id, "Select option", option); 
+  }
 });
 
 
-
+function register_user (){
+  pg.connect(process.env.DATABASE_URL, function(err,client,done) {
+    client.query('SELECT * FROM test_table',function(err,result) {
+      done();
+      if(err){
+        console.error(err); response.send("Error " + err); 
+      }
+      else{
+        response.render('pages/db',{results: result.rows} ); 
+      }
+    });
+  });
+}
