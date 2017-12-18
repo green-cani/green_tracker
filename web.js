c4d5f113fc4549c9d95d9fbf9128813b3586c7ff
inknos <nicola.sella@outlook.it>
2017-12-18 19:50:46 +0100

    added lines to web.js from heroku docs on node.js

--- web.js
+++ web.js
@@ -5,10 +5,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
-
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
